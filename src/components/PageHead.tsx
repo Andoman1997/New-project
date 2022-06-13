bb2bfeb9bdcfb1c557082d9098958c83950764ab
Andrey <andrey.manukyan@brl.ru>
2022-06-13 16:33:20 +0400

    .

---
+++ src/components/PageHead.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+function PageHead() {
+    return (
+        <div>
+            <Head>
+                <title>Provineer</title>
+                <meta name="description" content="Meta description for the  page" />
+
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+            </Head>
+        </div>
+    );
+}
+
+export default PageHead;
