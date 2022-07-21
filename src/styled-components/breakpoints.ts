a12f8bdf6f623cb76636d4b5a5115838f66c0c56
Andrey <andrey.manukyan@brl.ru>
2022-07-22 00:10:21 +0400

    adding breakpoints

---
+++ src/styled-components/breakpoints.ts
@@ -0,0 +1,67 @@
+function createBreakpoints() {
+  const { values, unit, step } = {
+    values: {
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+    unit: 'px',
+    step: 5,
+  };
+  const keys = Object.keys(values) as (keyof typeof values)[];
+
+  function up(key: keyof typeof values) {
+    const value = typeof values[key] === 'number' ? values[key] : key;
+    return `@media (min-width:${value}${unit})`;
+  }
+
+  function down(key: keyof typeof values) {
+    const endIndex = keys.indexOf(key) + 1;
+    const upperbound = values[keys[endIndex]];
+
+    if (endIndex === keys.length) {
+      return up('sm');
+    }
+
+    const value = endIndex > 0 ? upperbound : values[key];
+    return `@media (max-width:${value - step / 100}${unit})`;
+  }
+
+  function between(start: keyof typeof values, end: keyof typeof values) {
+    const endIndex = keys.indexOf(end);
+
+    if (endIndex === keys.length - 1) {
+      return up(start);
+    }
+
+    return (
+      `@media (min-width:${values[start]}${unit}) and ` +
+      `(max-width:${
+        (endIndex !== -1 ? values[keys[endIndex + 1]] : values[end]) - step / 100
+      }${unit})`
+    );
+  }
+
+  function only(key: keyof typeof values) {
+    return between(key, key);
+  }
+
+  function width(key: keyof typeof values) {
+    return values[key];
+  }
+
+  return {
+    keys,
+    values,
+    up,
+    down,
+    between,
+    only,
+    width,
+  };
+}
+
+const bp = createBreakpoints();
+
+export default bp;
--- src/styled-components/theme.ts
+++ src/styled-components/theme.ts
@@ -2,12 +2,14 @@ import typography from './typography';
 import colors from './colors';
 import radius from './radius';
 import indents from './indents';
+import breakpoints from './breakpoints';
 
 export const theme = {
   typography,
   colors,
   radius,
   indents,
+  breakpoints,
 };
 
 export type ThemeInterface = typeof theme;
