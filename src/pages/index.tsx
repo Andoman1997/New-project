a3363835a909b6898488f12ba7747cf4bd51d30f
Andrey <andrey.manukyan@brl.ru>
2022-07-21 20:48:19 +0400

    fix file import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import PageHead from 'components/PageHead';
+import PageHead from '../containers/PageHead';
 import Header from 'components/../containers/Navbar/Navbar';
 
 const Home: NextPage = () => (
