ec4f5670a11a040720cfaaead8186866a85b9561
Andrey <andrey.manukyan@brl.ru>
2022-07-21 16:08:31 +0400

    creating GlobalStyle component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import '/src/components/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { GlobalStyle } from '../styled-components';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
--- src/styled-components/index.ts
+++ src/styled-components/index.ts
@@ -1,18 +1,22 @@
-html,
-body {
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
     padding: 0;
     margin: 0;
     overflow: hidden;
     background-color: #FCFCFC;
     font-family: Satoshi-variable, sans-serif;
-}
+  }
 
-a {
+  a {
     color: inherit;
     text-decoration: none;
-}
+  }
 
-@font-face {
+  @font-face {
     font-family: Satoshi-variable;
     src: url(/public/static/fonts/Satoshi-Variable.ttf);
-}
+  }
+`;
