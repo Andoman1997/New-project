3293380d79a0f9c67749c47b6c7c146e9c2f571f
Andrey <andrey.manukyan@brl.ru>
2022-07-29 13:02:49 +0400

    href name fix

--- src/containers/NavLinks.tsx
+++ src/containers/NavLinks.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const NavLinks = [
-  { name: 'How It Works', to: 'how-it-Works' },
+  { name: 'How It Works', to: 'how-it-works' },
   { name: 'About', to: '/about' },
   { name: 'Support', to: '/support' },
 ];
