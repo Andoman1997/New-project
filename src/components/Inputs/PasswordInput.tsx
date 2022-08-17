fc3c24eceb3ce3aa765cb2e40159fb4ac4a3ae65
Andrey <andrey.manukyan@brl.ru>
2022-08-17 22:28:41 +0400

    delete custom hook, and create Password separate input

---
+++ src/components/Inputs/PasswordInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
+import { InfoInput, InfoInputWrapper } from 'src/pages/signup/styled';
+
+export const EyeIconButton = styled.button`
+  position: absolute;
+  top: 16px;
+  right: 10px;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  background: none;
+`;
+
+interface PassProps {
+  label: string;
+}
+
+const PasswordInput: React.FC<PassProps> = ({ label }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggle = (e: React.MouseEvent<HTMLElement>): void => {
+    e.preventDefault();
+    setShowPassword(prev => !prev);
+  };
+
+  return (
+    <InfoInputWrapper>
+      <InfoInput type={!showPassword ? 'password' : 'text'} placeholder=" " />
+      <label>{label}</label>
+
+      <EyeIconButton onClick={toggle}>
+        {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+      </EyeIconButton>
+    </InfoInputWrapper>
+  );
+};
+
+export default PasswordInput;
